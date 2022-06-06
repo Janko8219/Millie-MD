b58922071e9e8b140c3af5167c3a658401ddf4e2
Janko8219 <98521160+Janko8219@users.noreply.github.com>
2022-06-06 14:12:19 +0530

    Update credits.js

--- plugins/credits.js
+++ plugins/credits.js
@@ -1,22 +1,22 @@
 let handler = async (m, { usedPrefix}) => {
 let credit = `
-\t\t\t\t\t\t\t*THANKS TO*
+\t\t\t\t\t\t\t*THANKS TO ALL*
 
-*TOXIC ALIEN:*
-*https://github.com/Alien-alfa*
 
-*D-3-V-1-L:*
-*https://github.com/D-3-V-1-L*
 
-*Toxic-Ajmal:*
-*https://github.com/Ajmal-Achu*
 
-*Lord-Official:*
-*https://github.com/Lord-official*
 
-*AND MYSELF*
+
+
+
+
+
+
+
+
+
 `
- conn.sendTBI(m.chat, credit, wm, 'https://i.pinimg.com/474x/de/bd/b2/debdb24645169bf95eecd49f3144315f.jpg', `SOURCE CODE`, `https://github.com/Neeraj-x0/Millie-MD`, null, null, 'Menu', `${usedPrefix}menu`, null, null, null, null, m) 
+ conn.sendTBI(m.chat, credit, wm, 'https://i.imgur.com/71XYObV.jpeg', `INSTA`, `http://instagram.com/_munavir._`, null, null, 'Menu', `${usedPrefix}menu`, null, null, null, null, m) 
 }
 handler.help = ['credits']
 handler.tags = ['info']
